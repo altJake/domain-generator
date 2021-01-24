9008335d261ffb0e15fd4771324ce8c9de4114ba
Jakob (Koby) Shimony <kobyshi@gmail.com>
2021-01-24 22:01:44 +0100

    naive implementation for multiple inputs

--- pkg/suggester/suggester.go
+++ pkg/suggester/suggester.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"strings"
 	"sync"
-
-	"github.com/pkg/errors"
 )
 
 // Suggester represents a sug suggestion finder
@@ -48,26 +46,28 @@ func (sug *Suggester) init() {
 
 // Suggest returns potential domains for the given input
 // Output is a map where keys are potential tlds and values are the full potential domain name
-func (sug *Suggester) Suggest(input string) (map[string]string, error) {
-	if len(input) < 4 {
-		return nil, errors.New("Top Level Domain cannot contain less than 2 letters, structure must be ab.cd at the least")
-	}
-
-	input = strings.ToLower(input)
-	collection := make(map[string]string)
-
-	tld := input[len(input)-2:]
-	secondaryLevel := input[:len(input)-len(tld)]
-	for len(secondaryLevel) >= 2 {
-		if sug.isExistingTLD(tld) {
-			collection[tld] = fmt.Sprintf("%s.%s", secondaryLevel, tld)
+func (sug *Suggester) Suggest(input ...string) (map[string]string, error) {
+	suggestions := make(map[string]string)
+	for _, in := range input {
+		if len(in) < 4 {
+			continue
 		}
 
-		tld = input[len(input)-(len(tld)+1):]
-		secondaryLevel = input[:len(input)-len(tld)]
+		tld := strings.ToLower(in)
+		tld = in[len(in)-2:]
+		secondaryLevel := in[:len(in)-len(tld)]
+		for len(secondaryLevel) >= 2 {
+			if sug.isExistingTLD(tld) {
+				suggestions[tld] = fmt.Sprintf("%s.%s", secondaryLevel, tld)
+			}
+
+			tld = in[len(in)-(len(tld)+1):]
+			secondaryLevel = in[:len(in)-len(tld)]
+		}
+
 	}
 
-	return collection, nil
+	return suggestions, nil
 }
 
 func (sug *Suggester) isExistingTLD(potentialTLD string) bool {
